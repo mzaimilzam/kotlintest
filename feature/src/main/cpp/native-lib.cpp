76309725bc5ca2291239d869015ae38402a32c7a
mzaimilzam <mzaimilzam10@gmail.com>
2018-12-10 17:19:51 +0700

    firts

---
+++ feature/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_zaim_kotlin1_feature_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
